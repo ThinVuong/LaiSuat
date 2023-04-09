0a013d47130747cbd697688fc1ce2c4918151cfd
hv <hv@PM309-15>
2023-04-09 11:06:56 +0700

    Add project files.

---
+++ Controllers/LaiSuatController.cs
@@ -0,0 +1,26 @@
+﻿using LaiSuat.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaiSuat.Controllers
+{
+    public class LaiSuatController : Controller
+    {
+        
+        public IActionResult LaiSuat()
+        {
+
+            ModelBankInterest ls = new ModelBankInterest();
+
+            return View(ls);
+        }
+
+        [HttpPost]
+        public IActionResult LaiSuat(uint soTien, double laiSuat, uint kyHan)
+        {
+            uint tien_lai = (uint)(soTien * laiSuat / 1200 * kyHan);
+            uint tong_tien = (uint)(soTien + tien_lai);
+           ModelBankInterest ls= new ModelBankInterest(soTien.ToString("N0"), laiSuat.ToString("N0"), kyHan.ToString("N0"), tien_lai.ToString("N0"), tong_tien.ToString("N0"));
+            return View(ls);
+        }
+    }
+}
---
+++ Models/ModelBankInterest.cs
@@ -0,0 +1,19 @@
+﻿namespace LaiSuat.Models
+{
+    public class ModelBankInterest
+    {
+        public string SoTienGui { get; set; }
+        public string LaiSuatGui { get; set; }
+        public string KyHanGui { get; set; }
+        public string TienLai { get; set; }
+        public string TongTien { get; set; }
+        public ModelBankInterest(string soTienGui = "you fool", string laiSuatGui = "how could you", string kyHanGui = "I belied in you", string tienLai = "You've failed me", string tongTien = "face my wrath")
+        {
+            SoTienGui = soTienGui;
+            LaiSuatGui = laiSuatGui;
+            KyHanGui = kyHanGui;
+            TienLai = tienLai;
+            TongTien = tongTien;
+        }
+    }
+}
